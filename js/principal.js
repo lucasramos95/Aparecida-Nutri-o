9fe57d74693430a6e6d35496e5f9fad987ad6210
Lucas <lucas_ramos95@outlook.com>
2022-12-06 23:55:27 -0300

    commit #03 - variavel de cálculo de Imc

--- js/principal.js
+++ js/principal.js
@@ -9,4 +9,8 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura;
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
