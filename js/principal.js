d9dca59dd9916a96a0cffecf02ed171d8f63a018
Lucas <lucas_ramos95@outlook.com>
2022-12-09 15:01:00 -0300

    commit #06 - Alteração de background/style via JS e CSS, em caso de imc inválido

--- js/principal.js
+++ js/principal.js
@@ -22,12 +22,14 @@ if(peso <= 0 || peso >= 1000){
     console.log("Peso inválido!");
     pesoEhValido = false;
     tdImc.textContent = "Peso inválido";
+    paciente.classList.add("paciente-invalido");
 }
 
 if(altura <= 0 || altura >= 3.00){
     console.log("Altura inválida!");
     alturaEhValida = false;
     tdImc.textContent = "Altura inválida!";
+    paciente.classList.add("paciente-invalido");
 }
 
 if(alturaEhValida && pesoEhValido){
