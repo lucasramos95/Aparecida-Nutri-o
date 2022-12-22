c7f9137f499309d8c4f1d82d119352632dffeba7
Lucas <lucas_ramos95@outlook.com>
2022-12-21 22:44:51 -0300

    commit #15 - código refatorado/aprimorado - form/imc

--- js/form.js
+++ js/form.js
@@ -7,6 +7,11 @@ botaoAdicionar.addEventListener("click", function(event) {
 
     var pacienteTr = montaTr(paciente);
 
+    if(!validaPaciente(paciente)){
+        console.log("Paciente inválido");
+        return;
+    }
+
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
@@ -47,4 +52,12 @@ function montaTd(dado,classe) {
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+    if(validaPeso(paciente.peso)){
+        return true;
+    }else{
+        return false;
+    }
 }
